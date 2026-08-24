67febc3030a00cf08f434bba19033ef142be16ac
Brasonite <gabriel.silva.araujo@proton.me>
2026-08-24 16:28:59 -0300

    Feat: Weapon now flips when aiming left

--- src/entities/player/weapon.gd
+++ src/entities/player/weapon.gd
@@ -10,6 +10,11 @@ func _process(_delta: float) -> void:
 	var angle := atan2(relative_mouse_pos.y, relative_mouse_pos.x)
 	_weapon_axis.rotation = angle
 	
+	if absf(angle) >= PI / 2.0:
+		_weapon.scale.y = -1.0
+	else:
+		_weapon.scale.y = 1.0
+	
 	var distance := relative_mouse_pos.length()
 	_weapon.position.x = clampf(distance - _weapon.length, 0.0, weapon_distance)
 
